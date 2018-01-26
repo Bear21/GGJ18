8e29a196cb3eecbfa68157678f7ab746061bfa47
Bear21 <bear@8bitbear.com>
2018-01-26 21:49:09 +1100

    Added missing assets

---
+++ Not-a-gun/Assets/TriggerCondition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class TriggerCondition : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    bool TestConditio
+}
+
+public class HitTestCondition : TriggerCondition
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Not-a-gun/Assets/correctheight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class correctheight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = new Vector3(0, -0.5f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
