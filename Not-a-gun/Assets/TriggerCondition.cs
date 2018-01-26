a58e134aeee280244b2a9f0082c7ff3af98d7a5f
Bear21 <bear@8bitbear.com>
2018-01-26 23:42:11 +1100

    Added working buttons etc

--- Not-a-gun/Assets/TriggerCondition.cs
+++ Not-a-gun/Assets/TriggerCondition.cs
@@ -2,31 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-abstract public class TriggerCondition : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    bool TestConditio
-}
-
-public class HitTestCondition : TriggerCondition
-{
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+interface ITriggerCondition {
+    bool GetTriggered();
 }
