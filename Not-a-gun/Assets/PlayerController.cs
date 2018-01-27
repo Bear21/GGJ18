245189284d70bb66daf851a51b6f8c88fe2758ee
Bear21 <bear@8bitbear.com>
2018-01-27 12:24:04 +1100

    Added Character model

--- Not-a-gun/Assets/PlayerController.cs
+++ Not-a-gun/Assets/PlayerController.cs
@@ -5,18 +5,30 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
-
+    private Rigidbody rb;
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        Vector3 moveDirection = new Vector3(0,0,0);
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded) {
+            var x = Input.GetAxis("Horizontal");
+            var z = Input.GetAxis("Vertical");
+            moveDirection = Vector3.Normalize(x * transform.right + z * transform.forward);
+            moveDirection *= 100;
+            // if (Input.GetButton("Jump"))
+            //     moveDirection.y = jumpSpeed;
+            
+        }
+        moveDirection.y += UnityEngine.Physics.gravity.y * Time.deltaTime;
+        controller.SimpleMove(moveDirection * Time.deltaTime);
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        //rb.AddForce(new Vector3(x, 0, z), ForceMode.Acceleration);
+        //transform.Rotate(0, x, 0);
+        //transform.Translate(0, 0, z);
     }
 }
