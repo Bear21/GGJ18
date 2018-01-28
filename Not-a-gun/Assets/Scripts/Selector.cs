a851fea2f39db49abc00f7c5ecc87091b177d765
Bear21 <bear@8bitbear.com>
2018-01-28 16:36:40 +1100

    Hacked in drop point

--- Not-a-gun/Assets/Scripts/Selector.cs
+++ Not-a-gun/Assets/Scripts/Selector.cs
@@ -28,51 +28,60 @@ public class Selector : NetworkBehaviour {
       {
          storage.transform.Rotate(new Vector3(0.3f, 0.2f, 0.1f), 100 * Time.deltaTime);
       }
-      if (!isLocalPlayer || !Input.GetMouseButton(0))
+      if (!isLocalPlayer || !Input.GetMouseButtonDown(0))
          return;
 
       RaycastHit hit;
       if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit, LayerMask.GetMask("Selectable", "Wall")))
          return;
-      Transform hitTransform = null;
+      Transform hitTransform = hit.transform;
       if (hit.transform.GetComponentInChildren<EmptyScript>())
       {
          Debug.Log(string.Format("can find: {0}", hit.transform.GetComponentInChildren<EmptyScript>().name));//
          hitTransform = hit.transform.GetComponentInChildren<EmptyScript>().transform;
       }
-      if (hitTransform == null) { return; }
+      if (hitTransform == null || hitTransform.tag == "Player") { return; }
       Renderer rend = hitTransform.GetComponent<Renderer>();
       //MeshCollider meshCollider = hit.collider as MeshCollider;
 
       
       GameObject obj;
-      if (rend == null || rend.sharedMaterial == null || hitTransform.tag != "Selectable") {
+      if (rend == null || rend.sharedMaterial == null || (hitTransform.tag != "Selectable" && hitTransform.tag != "DropPoint")) {
             return;
       }
       obj = hitTransform.gameObject;
-      //Destroy(hit.transform.gameObject);
 
-      bool result = Physics.Raycast(obj.transform.position, Vector3.Normalize(storagePoint.position - obj.transform.position), out hit, Vector3.Distance(storagePoint.position, obj.transform.position));
-      hitTransform = null;
+      bool result = Physics.Raycast(storagePoint.position, Vector3.Normalize(storagePoint.position - obj.transform.position), out hit, Vector3.Distance(storagePoint.position, obj.transform.position));
       if (result)
       {
+         hitTransform = hit.transform;
          if (hit.transform.GetComponentInChildren<EmptyScript>() != null)
          {
-            Debug.Log(string.Format("can find: {0}", hit.transform.GetComponentInChildren<EmptyScript>().name));//
+            Debug.Log(string.Format("can find: {0}", hit.transform.GetComponentInChildren<EmptyScript>().name));
             hitTransform = hit.transform.GetComponentInChildren<EmptyScript>().transform;
          }
-      
+         if (hitTransform.tag == "DropPoint")
+         {
+            CmdDrop(hitTransform.gameObject);
+         }
          if (hitTransform.tag != "Selectable")
          {
             return;
          }
          obj = hitTransform.gameObject;
       }
-      
+
 
       //obj.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
-
-      CmdStorage(obj);
+      if (hitTransform.tag == "DropPoint")
+      {
+         CmdDrop(hitTransform.gameObject);
+      }
+      else
+      {
+         if (obj == null) return;
+         CmdStorage(obj);
+      }
       //obj.transform.position = storagePoint.position;
 
 
@@ -94,12 +103,28 @@ public class Selector : NetworkBehaviour {
    {
       if (obj.transform.parent != null)
       {
-         obj.transform.parent.GetComponentInParent<Selector>().storage = null;
+         Selector selector = obj.transform.parent.GetComponentInParent<Selector>();
+         if (selector != null)
+            obj.transform.parent.GetComponentInParent<Selector>().storage = null; 
       }
       obj.transform.SetParent(storagePoint);
       obj.transform.position = storagePoint.position;
       obj.transform.localScale = new Vector3(1, 1, 1);
+      storage = obj;
       Debug.Log("Transfered");
+   }
+
+   private void Drop(GameObject obj)
+   {
+      if (storage == null)
+      {
+         return;
+      }
+      storage.transform.SetParent(obj.transform);
+      storage.transform.position = obj.transform.position;
+      storage.transform.localScale = obj.transform.localScale;
+      Debug.Log("Dropped");
+      storage = null;
    }
 
    [Command]
@@ -113,6 +138,19 @@ public class Selector : NetworkBehaviour {
    void RpcStorage(GameObject obj)
    {
       transmit(obj);
+   }
+
+   [Command]
+   public void CmdDrop(GameObject obj)
+   {
+      Drop(obj);
+      RpcDrop(obj);
+   }
+
+   [ClientRpc]
+   void RpcDrop(GameObject obj)
+   {
+      Drop(obj);
    }
 
    //void storageHook(GameObject obj)
